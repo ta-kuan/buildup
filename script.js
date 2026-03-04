da7231a914bf58c4b9d14581418dcb17d078e7e4
ta-kuan <176125305@neg.edu.pref.shizuoka.jp>
2026-03-04 22:52:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
 
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('bgA', 'backr.png');
+  this.load.image('bgB', 'backb.png');
+  this.load.image('btnA', 'buttona.png');
+  this.load.image('btnB', 'buttonb.png');
+}
+
+function create() {
+
+  const width = this.scale.width;
+  const height = this.scale.height;
+
+  let bgA = this.add.image(0, 0, 'bgA')
+    .setOrigin(0)
+    .setDisplaySize(width, height)
+    .setAlpha(1);
+
+  let bgB = this.add.image(0, 0, 'bgB')
+    .setOrigin(0)
+    .setDisplaySize(width, height)
+    .setAlpha(0);
+
+  let buttonA = this.add.image(width / 2 - 150, height / 2, 'btnA')
+    .setInteractive();
+
+  let buttonB = this.add.image(width / 2 + 150, height / 2, 'btnB')
+    .setInteractive();
+
+  function switchToB() {
+
+    buttonA.destroy();
+    buttonB.destroy();
+
+    this.tweens.add({
+      targets: bgA,
+      alpha: 0,
+      duration: 500
+    });
+
+    this.tweens.add({
+      targets: bgB,
+      alpha: 1,
+      duration: 500
+    });
+  }
+
+  buttonA.on('pointerdown', switchToB, this);
+  buttonB.on('pointerdown', switchToB, this);
+}
